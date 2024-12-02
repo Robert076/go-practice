5e30552e5ee1777e8e4943e98a6217886342ddad
Robert076 <robertberes06@gmail.com>
2024-12-02 12:24:36 +0200

    feat: initial code

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	// go is statically typed
+	// meaning that variables always have a specific type and cannot change
+	var x string = "Hello world!"
+	fmt.Println(x)
+}
