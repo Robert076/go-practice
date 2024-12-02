9ea3d3e8e1f7f5ff18a91f4ab4c772d82cab3942
Robert076 <robertberes06@gmail.com>
2024-12-02 12:27:22 +0200

    feat: reflect

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 	// go is statically typed
 	// meaning that variables always have a specific type and cannot change
 	var x string = "Hello world!"
 	fmt.Println(x)
+
+	var y string
+	y = "Test"
+	fmt.Println(y)
+
+	var z = "Hello"
+	z += " World!"
+	fmt.Println(reflect.TypeOf(z)) // instanceof from java
 }
